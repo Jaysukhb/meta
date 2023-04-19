300be5007d97aa8505d76b173eaac0a79c1bde7d
y2meta <y2meta@proton.me>
2023-04-19 18:27:59 +0530

    Updates

---
+++ component---src-pages-404-js-bba451018330ab576769.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[8883],{429:function(e,t,a){a.r(t);var s=a(7294);t.default=()=>s.createElement("div",null,s.createElement("h1",null,"404: Not Found"),s.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-bba451018330ab576769.js.map
